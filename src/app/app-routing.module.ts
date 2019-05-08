e489c67ee7ef40a7fed194ee265bd441aa7da56c
michael <glacieraaa@gmail.com>
2019-05-07 23:02:23 -0400

    changed route module and add product-detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './product-list/product-detail/product-detail.component';
 
 const routes: Routes = [
-  { path:'', component: HomeComponent }
+  { path:'', component: HomeComponent },
+  { path: 'products/:productID', component: ProductDetailComponent }
 ];
 
 @NgModule({

ba06e67fa96053b83a5af2263a0af7af06bc5773
michael <glacieraaa@gmail.com>
2019-05-11 16:36:59 -0400

    changed footer component to be fully responsive; finished HTML for login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
-import { ProductPageComponent } from './product-list/product-page/product-page.component';
-import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { SignupComponent } from './auth/signup/signup.component';
+import { ProductPageComponent } from './product-list/product-page/product-page.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
+
 
 
 const routes: Routes = [
   { path:'', component: HomeComponent },
-  { path: 'products/:productId', component: ProductPageComponent }
+  { path: 'products/:productId', component: ProductPageComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({

3a13ba482bb56527f7e6557784e88a9c7fd310c0
michael <glacieraaa@gmail.com>
2019-05-17 17:09:34 -0400

    added user model to backend

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,9 @@
-suffering
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true})
+  .then(
+    () => { console.log('connection succeeded.') },
+    err => { console.log('connection failed.') }
+  );
